e0d305407131e0ddd31d8eae98a9d59f19f0b84d
dfiore1230 <dfiore1230@users.noreply.github.com>
2015-12-15 14:10:22 -0500

    fixed spacing

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function postToTrello(listId, command, text, user_name, cb) {
 	var card_data = {
 		'name' : name_and_desc.shift() + ' (@' + user_name + ')',
 		'desc' : name_and_desc.shift(),
-    'list_id' : name_and_desc.shift()
+    		'list_id' : name_and_desc.shift()
 	};
 
 	trello.post('/1/lists/' + listId + '/cards', card_data, cb);
