33af5d90bf6af03afd46949ee74b196fddd09f23
David Fiore <david.fiore@gmail.com>
2015-12-16 17:26:56 -0500

    i need some classes on this shit

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ function postToTrello(listId, command, text, user_name, cb) {
     // If none set, default to blog
     listName = '5670696fa98d9db94c818c5a';
   }
-  else if (card_data.list_name == "blog") {
+  else if (card_data.list_name == blog) {
     listName = '5670696fa98d9db94c818c5a';
   }
-  else if (card_data.list_name == "done") {
+  else if (card_data.list_name == done) {
     listName = '5670696d37e05b451fe05482';
   }
 
