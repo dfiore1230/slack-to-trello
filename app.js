d0807f09c3e4115ad74c357cb65f119d6cab45bc
David Fiore <david.fiore@gmail.com>
2015-12-15 21:29:47 -0500

    i am not good at my job, wait this isn't my job

--- app.js
+++ app.js
@@ -27,17 +27,20 @@ function postToTrello(listId, command, text, user_name, cb) {
 }
 
 // Set Fallback list_id
-var list_id = '5670696fa98d9db94c818c5a';
+//var list_id = '5670696fa98d9db94c818c5a';
 
 function list_check(list_id) {
-  if (card_data[2] == blog) {
+  if (list_name == blog) {
       var list_id = '5670696fa98d9db94c818c5a';
       //throw new console.log('list id set to 5670696fa98d9db94c818c5a');
     }
-  else if (card_data[2] == done) {
+  else if (list_name == done) {
       var list_id = '5670696d37e05b451fe05482';
       //throw new console.log('list id set to 5670696d37e05b451fe05482');
     }
+  else {
+    var list_id = '5670696fa98d9db94c818c5a';
+  }
 }
 
 app.post('/*', function(req, res, next) {
