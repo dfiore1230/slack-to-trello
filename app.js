a73a13af719f348c2a9b1b91ef0fa5028a8c3f55
David Fiore <david.fiore@gmail.com>
2015-12-15 21:36:17 -0500

    yesssssss

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function postToTrello(listId, command, text, user_name, cb) {
 }
 
 // Set Fallback list_id
-var list_id = null;
+var list_id;
 
 function list_check(list_id) {
   if (list_name("blog")) {
