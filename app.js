749e1616e6a865726e996345640ccca54c3d91b0
David Fiore <david.fiore@gmail.com>
2015-12-15 21:51:59 -0500

    take 2098454

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function postToTrello(listId, command, text, user_name, cb) {
 	trello.post('/1/lists/' + listId + '/cards', card_data, cb);
 }
 
-console.log("Card Data 2 is" + card_data[2]);
+console.log('Card Data 2 is' + card_data[2]);
 
 // Set Fallback list_id
 var list_id = '5670696fa98d9db94c818c5a';
